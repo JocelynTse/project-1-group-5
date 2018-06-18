8f12cbb01aacf9260672446e69bcde8a2eb38c36
George Jacob Work <36488006+georgejacobt@users.noreply.github.com>
2018-06-18 13:47:07 -0500

    materialize tests

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+
+
+$( document ).ready(function() {
+   
+    $(".dropdown-trigger").dropdown({ hover: true,
+                                    coverTrigger: false,
+                                     alignment: "right" }
+                                    
+                                    );
+
+
+     $('.sidenav').sidenav({edge: "right"});
+                                });
+
+    $('.carousel.carousel-slider').carousel({
+         fullWidth: true
+     });
