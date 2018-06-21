a64d87c4e10fce0288288a73034e353d8a16831c
George Jacob Work <36488006+georgejacobt@users.noreply.github.com>
2018-06-21 16:38:52 -0500

    added mediabox

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,6 +34,8 @@ $(document).ready(function () {
     //   selectMonths: true, // Creates a dropdown to control month
     //   selectYears: 15 // Creates a dropdown of 15 years to control year
     });
+
+    $('.materialboxed').materialbox();
 });
 
 $('.carousel.carousel-slider').carousel({
