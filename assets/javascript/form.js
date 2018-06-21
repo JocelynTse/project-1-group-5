da664f223c7233c58ad46c2a162cde118acfa395
JocelynTse <jocelyn.s.tse@gmail.com>
2018-06-21 11:49:40 -0500

    Added new JS file for form

---
+++ assets/javascript/form.js
@@ -0,0 +1,22 @@
+
+ // document.addEventListener('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('.datepicker');
+    //var instances = M.Datepicker.init(elems, options);
+ // });
+
+  //document.addEventListener('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('select');
+    //var instances = M.FormSelect.init(elems, options);
+ //});
+
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+          
