621199783547d90ce20596a18640218e27dea2f3
JocelynTse <jocelyn.s.tse@gmail.com>
2018-06-23 13:49:43 -0500

    Fixed errors

--- assets/javascript/storage.js
+++ assets/javascript/storage.js
@@ -6,58 +6,86 @@ var config = {
     projectId: "sportsreghub",
     storageBucket: "sportsreghub.appspot.com",
     messagingSenderId: "1003875518898"
-  };
-  firebase.initializeApp(config);
-  
-  let database = firebase.database();
-  
-  let name = "";
-  let dob = "";
-  let parent = "";
-  let phone = "";
-  
-  $("#submit").on("click", function (event) {
-    event.preventDefault();
-  
-    var nameNew = $("#nameFirstPlayer").val() + " " + $("#nameLastPlayer").val().trim();
-    var dobNew = $("#DOBPlayer").val();
-    var parentNew = $("#nameParent").val().trim() + " " + $("#nameLastEmer1").val().trim();
-    var phoneNew = $("#phoneNumberPlayer").val();
-  
-    console.log($("#nameFirstPlayer").val());
-    console.log($("#DOBPlayer").val());
+};
+firebase.initializeApp(config);
 
-    //name = $("#nameFirstPlayer").val();
+let database = firebase.database();
+
+let name = "";
+let address = "";
+let phone = "";
+let dob = "";
+let age = "";
+let gender = "";
+let parent = "";
+let parentPhone = "";
+let phoneType = "";
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    var nameNew = $("#nameFirstPlayer").val() + " " + $("#nameLastPlayer").val().trim();
+    var addressNew = $("#addressPlayer").val() + ", " + $("#cityPlayer").val() + ", " + $("#statePlayer").val();
+    var phoneNew = $("#phoneNumberPlayer").val();
+    var dobNew = $("#DOBPlayer").val();
+    var ageNew = $("#agePlayer").val();
+    var genderNew = $("#genderPlayer").val();
+    var parentNew = $("#nameFirstParent").val() + " " + $("#nameLastParent").val();
+    var parentPhoneNew = $("#phoneNumberParent").val();
+    var phoneTypeNew = $("#phoneTypeParent").val();
 
     console.log(nameNew);
+    console.log(addressNew);
+    console.log(phoneNew);
+    console.log(dobNew);
+    console.log(ageNew);
+    console.log(genderNew);
+    console.log(parentNew);
+    console.log(parentPhoneNew);
+    console.log(phoneTypeNew);
 
     database.ref().push({
-      name: nameNew,
-      dob: dobNew,
-      //parent: parentNew,
-      //phone: phoneNew,
+        name: nameNew,
+        address: addressNew,
+        phone: phoneNew,
+        dob: dobNew,
+        age: ageNew,
+        gender: genderNew,
+        parent: parentNew,
+        parentPhone: parentPhoneNew,
+        phoneType: phoneTypeNew,
+
     })
-  });
-  
-  database.ref().on("child_added", function (childSnapshot) {
-  
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+
     name = childSnapshot.val().name;
-    dob = childSnapshot.val().dob;
-    parent = childSnapshot.val().parent;
+    address = childSnapshot.val().address;
     phone = childSnapshot.val().phone;
-  
+    dob = childSnapshot.val().dob;
+    age = childSnapshot.val().age;
+    gender = childSnapshot.val().gender;
+    parent = childSnapshot.val().parent;
+    parentPhone = childSnapshot.val().parentPhone;
+    phoneType = childSnapshot.val().phoneType;
+
     let tRow = $("<tr>");
-  
+
     let nameTab = $("<td>").text(name);
-    let dobTab = $("<td>").text(dob);
-    let parentTab = $("<td>").text(parent);
+    let addressTab = $("<td>").text(address);
     let phoneTab = $("<td>").text(phone);
-  
-    tRow.append(nameTab, dobTab, parentTab, phoneTab);
+    let dobTab = $("<td>").text(dob);
+    let ageTab = $("<td>").text(age);
+    let genderTab = $("<td>").text(gender);
+    let parentTab = $("<td>").text(parent);
+    let parentPhoneTab = $("<td>").text(parentPhone);
+    let phoneTypeTab = $("<td>").text(phoneType);
+
+    tRow.append(nameTab, addressTab, phoneTab, dobTab, ageTab, genderTab, parentTab, parentPhoneTab, phoneTypeTab);
     $("tbody").append(tRow);
-  
-  }, function (errorObject) {
+
+}, function (errorObject) {
     console.log("Errors handled: " + errorObject.code)
-  });
-  
-  
+});
+
